1b103f6d8bc15a26aa74e46cdd2778db65bf79a5
dvaluev91 <dvaluev91@yandex.ru>
2024-04-25 22:40:32 +0300

    Добавьте файлы проекта.

---
+++ Module_7.Unit_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Module_7.Unit_1.Models;
+
+namespace Module_7.Unit_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
